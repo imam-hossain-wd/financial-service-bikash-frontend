1b881b6391fa5b7eaec3d59ca6b54d0dc627af1a
imam-hossain1 <zunayedemon6t9@gmail.com>
2024-02-28 03:24:24 +0600

    home page setup complete

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,22 @@
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-    
 
     return (
  
-        <div>
-            home
+        <div className="flex justify-around items-center -mt-10">
+           <div className="-mt-30">
+            <h1>Comprehensive Money Transfer <br /> Services</h1>
+            <p className="w-96">Experience hassle-free money transfers with our secure platform. Send, cash out, and cash in effortlessly, plus enjoy a range of additional services.</p>
+
+            <Button className="mt-5 w-32 h-10" type="primary"><Link to="/transation">
+            Getting Start</Link></Button>
+           </div>
+
+           <div>
+            <img className="w-[700px] h-[600px]" src="https://i.ibb.co/whKfdwp/Online-transactions-rafiki.png" alt="banner"/>
+           </div>
         </div>
     );
 };
